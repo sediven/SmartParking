b28e064c046142bad69a055c6e9ffbc19bb832ef
sediven <konrad5446@wp.pl>
2017-05-31 23:31:42 +0200

    fixevent

--- FlaskPark/FlaskPark.py
+++ FlaskPark/FlaskPark.py
@@ -12,7 +12,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 cars = []
 events_example = '{"eventId": {"car": {"carId": "1","dataOut": "2017-04-22 17:13:25","owner": "tata"},"dataNow": "2017-04-22 14:15:25","typ": "in"},"eventId2": {"car": {"carId": "1","dataIn": "2017-04-22 14:13:25","owner": "tata"},"dataNow": "2017-04-22 11:13:25","typ": "out"},"eventId3": {"car": {"carId": "1","dataIn": "2017-04-22 14:13:25","owner": "tata"},"dataNow": "2017-04-22 13:13:25","typ": "out"}}'
 parkings = []
-
+events = []
 
 @app.route('/parking_state/<int:park_id>', methods=['GET'])
 @cross_origin()
@@ -50,28 +50,28 @@ def new_parking():
 
 @app.route('/give_events', methods=['POST'])
 @cross_origin()
-def events():
+def give_events():
     if not request.json:
         abort(400)
-    events = ParserInput(request.json)
+    events.append( ParserInput(request.json))
     try:
-        events.parseMyLines()
+        events[0].parseMyLines()
     except:
-        abort(400)
-    cars = events.manageCars()
+        abort(401)
+    cars = events[0].manageCars()
     return jsonify({'parking': parkings[0].printMe(), 'id': 0}), 201
 
 
 @app.route('/give_next_time_event', methods=['GET'])
 @cross_origin()
 def it_is_event_time():
-    return jsonify({'event': events.getNextEvent()}), 201
+    return jsonify({'event': events[0].getNextEvent()}), 201
 
 
 @app.route('/do_next_event', methods=['GET'])
 @cross_origin()
 def do_event():
-    type, event = events.doNextEvent()
+    type, event = events[0].doNextEvent()
     car = [c for c in cars if c.getCarId() == event[1]['car']['carId']][0]
     if type:
         path = parkings[0].parkCar(car)
